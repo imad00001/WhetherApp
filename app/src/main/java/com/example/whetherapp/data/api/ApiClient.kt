7095fac85ecc069b9782a3ca0e3d84ca47b37b62
imad00001 <imadkhn230@gmail.com>
2026-02-11 00:40:26 +0530

    Api handling 1

---
+++ app/src/main/java/com/example/whetherapp/data/api/ApiClient.kt
@@ -0,0 +1,25 @@
+package com.example.whetherapp.data.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient{
+       private const val BASE_URL = "https://api.openweathermap.org/"
+
+       private val loggingInterceptor = HttpLoggingInterceptor().apply() {
+           level = HttpLoggingInterceptor.Level.BODY
+       }
+
+       private val okHttpClient = OkHttpClient.Builder()
+           .addInterceptor(loggingInterceptor)
+           .build()
+
+       val retrofit : Retrofit = Retrofit.Builder()
+           .baseUrl(BASE_URL)
+           .client(okHttpClient)
+           .addConverterFactory(GsonConverterFactory.create())
+           .build()
+
+}
---
+++ app/src/main/java/com/example/whetherapp/data/api/WhetherApi.kt
@@ -0,0 +1,13 @@
+package com.example.whetherapp.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface api {
+    @GET("data/2.5/weather")
+    suspend fun getWhetherByCity(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+        ): WeatherResponse
+}
